24846300a9f58a8b372941abbc25f0e3191d1ea3
David Kahle <david@kahle.io>
2025-11-19 13:53:57 -0600

    minor readme reflow, roxing

--- R/em_est.R
+++ R/em_est.R
@@ -1,19 +1,26 @@
 #' em_est
 #'
-#' Estimates the mean array and covariance matrices from an array of tensor variate normal draws using an expectation-maximization algorithm.
+#' Estimates the mean array and covariance matrices from an array of tensor
+#' variate normal draws using an expectation-maximization algorithm.
 #'
-#' @param draws An array containing the draws, where the first mode represents each draw.
-#' @param max_iter A max number of iterations to try to get covariance matrices that converge.
+#' @param draws An array containing the draws, where the first mode represents
+#'   each draw.
+#' @param max_iter A max number of iterations to try to get covariance matrices
+#'   that converge.
 #' @param tol A tolerance level to define the convergence of matrices.
 #'
-#' @return A list containing the estimated mean array and the list of covariance matrices.
+#' @return A list containing the estimated mean array and the list of covariance
+#'   matrices.
 #'
 #' @examples
+#'
+#' em_est(data)
+#'
 #' @export
-
 em_est <- function(draws, max_iter = 1000, tol = 1e-6, quiet = TRUE, model) {
-
-  if(!model %in% c("skewt", "vargamma", "invgamma", "genhyper")) stop("Not a valid model. Must be skewt, vargamma, invgamma, or genhyper")
+  if (!model %in% c("skewt", "vargamma", "invgamma", "genhyper")) {
+    stop("Not a valid model. Must be skewt, vargamma, invgamma, or genhyper")
+  }
 
   # get dim of input
   dims <- dim(draws)[-1]
@@ -22,23 +29,28 @@ em_est <- function(draws, max_iter = 1000, tol = 1e-6, quiet = TRUE, model) {
   n_star <- prod(dims)
 
   # Step 1: Initialize vals
-  mu <- apply(X = draws, MARGIN = 2:(num_dim+1), FUN = mean)
+  mu <- apply(X = draws, MARGIN = 2:(num_dim + 1), FUN = mean)
 
   skew <- array(rnorm(prod(dims)), dim = dims)
   sigmas <- lapply(dims, diag)
 
   # different params based on model
-  switch(model,
-         "skewt" = nu <- 10,
-         "vargamma" = gamma <- 6,
-         "invgamma" = kappa <- 10,
-         "genhyper" = {lambda <- 10; omega <- 10 })
+  switch(
+    model,
+    "skewt" = nu <- 10,
+    "vargamma" = gamma <- 6,
+    "invgamma" = kappa <- 10,
+    "genhyper" = {
+      lambda <- 10
+      omega <- 10
+    }
+  )
 
-  for(t in 1:max_iter) {
+  for (t in 1:max_iter) {
     # Step 2: Update a, b, c depending on expected values
     skew_compute <- skew
 
-    for(d in seq_along(sigmas)) {
+    for (d in seq_along(sigmas)) {
       skew_compute <- n_prod(skew_compute, chol2inv(chol(sigmas[[d]])), d)
     }
 
@@ -47,66 +59,84 @@ em_est <- function(draws, max_iter = 1000, tol = 1e-6, quiet = TRUE, model) {
     delta_vals <- rep(0, n)
 
     mu_array <- replicate(n, mu, simplify = "array") |>
-      aperm(c(num_dim+1, (1:(num_dim))))
+      aperm(c(num_dim + 1, (1:(num_dim))))
 
     centered <- draws - mu_array
 
-    for(i in 1:n) {
-
-      center_draw <- centered[i, , ,]
+    for (i in 1:n) {
+      center_draw <- centered[i, , , ]
 
       centered_compute <- center_draw
 
-      for(d in seq_along(sigmas)) {
-        centered_compute <- n_prod(centered_compute, chol2inv(chol(sigmas[[d]])), d)
+      for (d in seq_along(sigmas)) {
+        centered_compute <- n_prod(
+          centered_compute,
+          chol2inv(chol(sigmas[[d]])),
+          d
+        )
       }
       delta_vals[i] <- sum(center_draw * centered_compute)
     }
 
-    switch(model,
-           "skewt" = {
-             delta_vals <- delta_vals + nu
-             param_vals <- -(nu + n_star)/2},
-           "genhyper" = {
-             rho <- rho + omega
-             delta_vals <- delta_vals + omega
-             param_vals <- lambda - n_star/2
-            },
-           "vargamma" = {
-             rho <- rho + 2 * gamma
-             param_vals <- gamma - n_star/2
-            },
-           "invgamma" = {
-             rho <- rho + kappa^2
-             delta_vals <- delta_vals + 1
-             param_vals <- -(1 + n_star)/2
-           })
+    switch(
+      model,
+      "skewt" = {
+        delta_vals <- delta_vals + nu
+        param_vals <- -(nu + n_star) / 2
+      },
+      "genhyper" = {
+        rho <- rho + omega
+        delta_vals <- delta_vals + omega
+        param_vals <- lambda - n_star / 2
+      },
+      "vargamma" = {
+        rho <- rho + 2 * gamma
+        param_vals <- gamma - n_star / 2
+      },
+      "invgamma" = {
+        rho <- rho + kappa^2
+        delta_vals <- delta_vals + 1
+        param_vals <- -(1 + n_star) / 2
+      }
+    )
 
-    k_lambda_1 <- besselK(x = sqrt(rho * delta_vals),
-                          nu = param_vals + 1, expon.scaled = TRUE)
-    k_lambda <- besselK(x = sqrt(rho * delta_vals),
-                        nu = param_vals, expon.scaled = TRUE)
+    k_lambda_1 <- besselK(
+      x = sqrt(rho * delta_vals),
+      nu = param_vals + 1,
+      expon.scaled = TRUE
+    )
+    k_lambda <- besselK(
+      x = sqrt(rho * delta_vals),
+      nu = param_vals,
+      expon.scaled = TRUE
+    )
 
-    a <- sqrt(delta_vals/rho) * (k_lambda_1/k_lambda)
+    a <- sqrt(delta_vals / rho) * (k_lambda_1 / k_lambda)
 
-    b <- sqrt(rho/delta_vals) * (k_lambda_1/k_lambda) -
-         (2 * param_vals)/delta_vals
+    b <- sqrt(rho / delta_vals) *
+      (k_lambda_1 / k_lambda) -
+      (2 * param_vals) / delta_vals
 
     eps <- 1e-5
 
-    K_plus <- besselK(x = sqrt(rho * delta_vals), nu = param_vals + eps,
-                      expon.scaled = TRUE)
+    K_plus <- besselK(
+      x = sqrt(rho * delta_vals),
+      nu = param_vals + eps,
+      expon.scaled = TRUE
+    )
 
-    K_minus <- besselK(x = sqrt(rho * delta_vals), nu = param_vals - eps,
-                       expon.scaled = TRUE)
+    K_minus <- besselK(
+      x = sqrt(rho * delta_vals),
+      nu = param_vals - eps,
+      expon.scaled = TRUE
+    )
 
-    c <- 1/2 * log(delta_vals/rho) + (log(K_plus) - log(K_minus))/(2 * eps)
+    c <- 1/2 * log(delta_vals / rho) + (log(K_plus) - log(K_minus)) / (2*eps)
 
     # replace NaN or Inf values
     b[!is.finite(b)] <- 0
     c[!is.finite(c)] <- 0
 
-    #browser()
     # Step 3: Update mu, skew, params
     weight_mean <- mean(a) * b - 1
     weight_skew <- mean(b) - b
@@ -115,134 +145,222 @@ em_est <- function(draws, max_iter = 1000, tol = 1e-6, quiet = TRUE, model) {
     num_skew <- 0
 
     for (i in 1:n) {
-       num_mean <- num_mean + weight_mean[i] * draws[i, , ,]
-       num_skew <- num_skew + weight_skew[i] * draws[i, , ,]
+      num_mean <- num_mean + weight_mean[i] * draws[i, , , ]
+      num_skew <- num_skew + weight_skew[i] * draws[i, , , ]
     }
 
     den_mean <- sum(mean(a) * b) - n
 
-    new_mu <- num_mean/den_mean
+    new_mu <- num_mean / den_mean
 
     den_skew <- sum(a * mean(b)) - n
-    new_skew <- num_skew/den_skew
+    new_skew <- num_skew / den_skew
 
     new_sigmas <- sigmas
 
     # update params based on model
-    update_nu <- function(nu, b, c, n) log(nu/2) + 1 - digamma(nu/2) - 1/n * sum(b+c)
-    update_gamma <- function(gamma, a, c) log(gamma) + 1 - digamma(gamma) + mean(c) - mean(a)
+    update_nu <- function(nu, b, c, n) {
+      log(nu / 2) + 1 - digamma(nu / 2) - 1 / n * sum(b + c)
+    }
+    update_gamma <- function(gamma, a, c) {
+      log(gamma) + 1 - digamma(gamma) + mean(c) - mean(a)
+    }
 
-    switch(model,
-           "skewt" = {
-             new_nu <- uniroot(update_nu, interval = c(1e-3, 1e3),
-                               b = b, c = c, n = n)$root
-           },
-           "genhyper" = {
-             K_plus <- besselK(x = omega, nu = lambda + eps,
-                               expon.scaled = TRUE)
+    switch(
+      model,
+      "skewt" = {
+        new_nu <- uniroot(
+          update_nu,
+          interval = c(1e-3, 1e3),
+          b = b,
+          c = c,
+          n = n
+        )$root
+      },
+      "genhyper" = {
+        K_plus <- besselK(x = omega, nu = lambda + eps, expon.scaled = TRUE)
 
-             K_minus <- besselK(x = omega, nu = lambda - eps,
-                                expon.scaled = TRUE)
+        K_minus <- besselK(x = omega, nu = lambda - eps, expon.scaled = TRUE)
 
-             new_lambda <- mean(c) * lambda * 1/((log(K_plus) - log(K_minus))/(2 * eps))
+        new_lambda <- mean(c) *
+          lambda *
+          1 /
+          ((log(K_plus) - log(K_minus)) / (2 * eps))
 
-             R_lambda <-
-               besselK(x = omega, nu = new_lambda + 1, expon.scaled = TRUE)/
-               besselK(x = omega, nu = new_lambda, expon.scaled = TRUE)
+        R_lambda <-
+          besselK(x = omega, nu = new_lambda + 1, expon.scaled = TRUE) /
+          besselK(x = omega, nu = new_lambda, expon.scaled = TRUE)
 
-             R_neg_lambda <-
-               besselK(x = omega, nu = -new_lambda + 1, expon.scaled = TRUE)/
-               besselK(x = omega, nu = -new_lambda, expon.scaled = TRUE)
+        R_neg_lambda <-
+          besselK(x = omega, nu = -new_lambda + 1, expon.scaled = TRUE) /
+          besselK(x = omega, nu = -new_lambda, expon.scaled = TRUE)
 
-             first_deriv <- 1/2 * (R_lambda + R_neg_lambda - (mean(a) + mean(b)))
+        first_deriv <- 1 / 2 * (R_lambda + R_neg_lambda - (mean(a) + mean(b)))
 
-             second_deriv <- 1/2 * (R_lambda^2 - (1 + 2 * new_lambda)/omega * R_lambda - 1 +
-                                R_neg_lambda^2 - (1 - 2 * new_lambda)/omega * R_neg_lambda - 1)
+        second_deriv <- 1 /
+          2 *
+          (R_lambda^2 -
+            (1 + 2 * new_lambda) / omega * R_lambda -
+            1 +
+            R_neg_lambda^2 -
+            (1 - 2 * new_lambda) / omega * R_neg_lambda -
+            1)
 
-             new_omega <- omega - first_deriv/second_deriv
-           },
-           "vargamma" = {
-             new_gamma <- uniroot(update_gamma, interval = c(1e-3, 1e3),
-                                  a = a, c = c)$root
-           },
-           "invgamma" = {
-             new_kappa = n/(sum(a))
-           })
+        new_omega <- omega - first_deriv / second_deriv
+      },
+      "vargamma" = {
+        new_gamma <- uniroot(
+          update_gamma,
+          interval = c(1e-3, 1e3),
+          a = a,
+          c = c
+        )$root
+      },
+      "invgamma" = {
+        new_kappa = n / (sum(a))
+      }
+    )
 
     scale_prod <- 1
 
-  for (j in 1:num_dim) {
-    first <- 0; second <- 0; third <- 0; fourth <- 0
+    for (j in 1:num_dim) {
+      first <- 0
+      second <- 0
+      third <- 0
+      fourth <- 0
 
-    n_d <- dims[j]
+      n_d <- dims[j]
 
-    # covar estimate for mode j
-    sigma_j <- matrix(0, nrow = n_d, ncol = n_d)
+      # covar estimate for mode j
+      sigma_j <- matrix(0, nrow = n_d, ncol = n_d)
 
-    # other modes
-    other_modes <- (1:num_dim)[-j]
+      # other modes
+      other_modes <- (1:num_dim)[-j]
 
-    # Build the whitening operator
-    inv_others <- diag(1)
+      # Build the whitening operator
+      inv_others <- diag(1)
 
-    for(d in rev(other_modes)) {
-      inv_others <- kronecker(inv_others, chol2inv(chol(sigmas[[d]])))
+      for (d in rev(other_modes)) {
+        inv_others <- kronecker(inv_others, chol2inv(chol(sigmas[[d]])))
+      }
+
+      A_i <- matricization(new_skew, j)
+
+      for (i in 1:n) {
+        Xi_centered <- matricization(draws[i, , , ] - new_mu, j)
+
+        first <- first + b[i] * (Xi_centered %*% inv_others %*% t(Xi_centered))
+
+        second <- second + A_i %*% inv_others %*% t(Xi_centered)
+
+        third <- third + Xi_centered %*% inv_others %*% t(A_i)
+
+        fourth <- fourth + a[i] * A_i %*% inv_others %*% t(A_i)
+      }
+
+      sigma_j <- n_d / (n * n_star) * (first - second - third + fourth)
+
+      if (j < num_dim) {
+        sigma_j <- sigma_j / (sum(diag(sigma_j))) * n_d
+      }
+
+      new_sigmas[[j]] <- sigma_j
     }
 
-    A_i <- matricization(new_skew, j)
+    #new_mu <- rand_mu; new_skew <- new_skew; new_sigmas <- list(s1, s2, s3); new_nu <- 20
 
-    for(i in 1:n) {
-       Xi_centered <- matricization(draws[i , , ,] - new_mu, j)
+    # Step 5: Check convergence
 
-       first <- first + b[i] * (Xi_centered %*% inv_others %*% t(Xi_centered))
+    switch(
+      model,
+      "skewt" = mean_change <- mean_rel_change(
+        new_mu,
+        mu,
+        new_skew,
+        skew,
+        new_sigmas,
+        sigmas,
+        new_nu,
+        nu
+      ),
+      "genhyper" = {
+        mean_change <- mean_rel_change(
+          new_mu,
+          mu,
+          new_skew,
+          skew,
+          new_sigmas,
+          sigmas,
+          new_lambda,
+          lambda,
+          new_omega,
+          omega
+        )
+      },
+      "vargamma" = mean_change <- mean_rel_change(
+        new_mu,
+        mu,
+        new_skew,
+        skew,
+        new_sigmas,
+        sigmas,
+        new_gamma,
+        gamma
+      ),
+      "invgamma" = mean_change <- mean_rel_change(
+        new_mu,
+        mu,
+        new_skew,
+        skew,
+        new_sigmas,
+        sigmas,
+        new_kappa,
+        kappa
+      )
+    )
 
-       second <- second + A_i %*% inv_others %*% t(Xi_centered)
-
-       third <- third + Xi_centered %*% inv_others %*% t(A_i)
-
-       fourth <- fourth + a[i] * A_i %*% inv_others %*% t(A_i)
+    if (t %% 50 == 0 & !quiet) {
+      cat(sprintf(
+        "Iteration %d: mean relative change = %.3e\n",
+        t,
+        mean_change
+      ))
+      #print(list(mu = mu, skew = skew, sigmas = sigmas, nu = nu))
     }
 
-    sigma_j <- n_d/(n * n_star) * (first - second - third + fourth)
+    if (mean_change < tol) {
+      message("Converged at iteration ", t)
+      break
+    }
+    # update all parameters
+    mu <- new_mu
+    skew <- new_skew
+    sigmas <- new_sigmas
 
-    if(j < num_dim) sigma_j <- sigma_j/(sum(diag(sigma_j))) * n_d
-
-    new_sigmas[[j]] <- sigma_j
+    switch(
+      model,
+      "skewt" = nu <- new_nu,
+      "genhyper" = {
+        lambda <- new_lambda
+        omega <- new_omega
+      },
+      "vargamma" = gamma <- new_gamma,
+      "invgamma" = kappa <- new_kappa
+    )
   }
-
-   #new_mu <- rand_mu; new_skew <- new_skew; new_sigmas <- list(s1, s2, s3); new_nu <- 20
-
-   # Step 5: Check convergence
-
-   switch(model,
-          "skewt" = mean_change <- mean_rel_change(new_mu, mu, new_skew, skew, new_sigmas, sigmas, new_nu, nu),
-          "genhyper" = {mean_change <- mean_rel_change(new_mu, mu, new_skew, skew, new_sigmas, sigmas, new_lambda, lambda, new_omega, omega)},
-          "vargamma" = mean_change <- mean_rel_change(new_mu, mu, new_skew, skew, new_sigmas, sigmas, new_gamma, gamma),
-          "invgamma" = mean_change <- mean_rel_change(new_mu, mu, new_skew, skew, new_sigmas, sigmas, new_kappa, kappa))
-
-   if(t %% 50 == 0 & !quiet) {
-     cat(sprintf("Iteration %d: mean relative change = %.3e\n", t, mean_change))
-     #print(list(mu = mu, skew = skew, sigmas = sigmas, nu = nu))
-   }
-
-   if (mean_change < tol) {
-     message("Converged at iteration ", t)
-     break
-   }
-   # update all parameters
-   mu <- new_mu; skew <- new_skew; sigmas <- new_sigmas;
-
-   switch(model,
-          "skewt" = nu <- new_nu,
-          "genhyper" = {lambda <- new_lambda; omega <- new_omega},
-          "vargamma" = gamma <- new_gamma,
-          "invgamma" = kappa <- new_kappa)
-  }
-  switch(model,
-         "skewt" = list(mu = mu, skew = skew, sigmas = sigmas, nu = nu),
-         "genhyper" = list(mu = mu, skew = skew, sigmas = sigmas, lambda = lambda, omega = omega),
-         "vargamma" = list(mu = mu, skew = skew, sigmas = sigmas, gamma = gamma),
-         "invgamma" = list(mu = mu, skew = skew, sigmas = sigmas, kappa = kappa))
+  switch(
+    model,
+    "skewt" = list(mu = mu, skew = skew, sigmas = sigmas, nu = nu),
+    "genhyper" = list(
+      mu = mu,
+      skew = skew,
+      sigmas = sigmas,
+      lambda = lambda,
+      omega = omega
+    ),
+    "vargamma" = list(mu = mu, skew = skew, sigmas = sigmas, gamma = gamma),
+    "invgamma" = list(mu = mu, skew = skew, sigmas = sigmas, kappa = kappa)
+  )
 }
 
 # Less computationally intensive way to compute sigmas
--- README.md
+++ README.md
@@ -1,31 +1,27 @@
 
-<!-- README.md is generated from README.Rmd. Please edit that file -->
-
-# tensor_models
+# **tensormodels**
 
 Tensor operations, decompositions, and distributions in R.
 
-## Installation
-
-You can install the development version of tensormodels from
-[GitHub](https://github.com/) with:
-
 ``` r
-#install.packages("devtools")
-devtools::install_github("ankan12/tensormodels")
+library("tensormodels")
+library("tidyverse")
+old_digits <- getOption("digits")
+options(digits = 3)
 ```
 
 # Operations
 
-# n-mode prod
+## n-mode prod
 
 The n-mode product between a tensor and a matrix can be computed with
 `n_mode_prod`.
 
 ``` r
 a <- array(1:3, dim = c(3, 1, 1))
-b <- matrix(4:9, nrow = 2, ncol = 3)
-n_mode_prod(a, b, 1)
+x <- matrix(4:9, nrow = 2, ncol = 3)
+
+n_prod(a, x, 1)
 #> , , 1
 #> 
 #>      [,1]
@@ -33,29 +29,31 @@ n_mode_prod(a, b, 1)
 #> [2,]   46
 ```
 
-# nm-mode prod
+## nm-mode prod
 
 The nm-mode product between a tensor and a tensor can be computed with
-`nm-mode-prod`.
+`nm_prod()`.
 
 ``` r
 A <- matrix(c(1, 2, 3, 4), nrow = 2)
-b <- matrix(c(5, 6), nrow = 2)
-nm_prod(A, b, 1, 1)
+x <- matrix(c(5, 6), nrow = 2)
+
+nm_prod(A, x, 1, 1)
 #>      [,1]
 #> [1,]   17
 #> [2,]   39
 ```
 
-# tensor prod
+## [Tensor product](https://en.wikipedia.org/wiki/Tensor_product)
 
 The tensor product between a tensor and a tensor can be computed with
-`tensor_prod`.
+`tensor_prod()`.
 
 ``` r
 A <- matrix(c(1, 2, 3, 4), nrow = 2)
-b <- matrix(c(5, 6), nrow = 2)
-tensor_prod(A, b)
+x <- matrix(c(5, 6), nrow = 2)
+
+tensor_prod(A, x)
 #> , , 1
 #> 
 #>      [,1] [,2]
@@ -69,9 +67,9 @@ tensor_prod(A, b)
 #> [2,]   12   24
 ```
 
-# Simulating Tensor Variate Normal Draws
+# Simulating tensor variate normal draws
 
-The function `rtnorm` simulates from the tensor variate normal with a
+The function `rtnorm()` simulates from the tensor variate normal with a
 specified mean array mu and a list of covariance matrices called
 list_sigmas.
 
@@ -81,27 +79,30 @@ well. Here, we simulate from a univariate normal with mean $-2$ and
 variance $4$.
 
 ``` r
-univar_draws <- rtnorm(n = 10000, mu = -2, list_sigmas = 4)
+univar_draws <- rtnorm(n = 1000, mu = -2, sigmas = 4)
+
 mean(univar_draws)
-#> [1] -1.984154
+#> [1] -2.12
+
 sd(univar_draws)
-#> [1] 2.007343
+#> [1] 1.99
 ```
 
 And now we simulate from the matrix variate normal of size $2 \times 3$.
 By default, the covariance matrices will be the identity.
 
 ``` r
-matrix_draws <- rtnorm(n = 10000, mu = matrix(1:6, nrow = 2, ncol = 3))
+matrix_draws <- rtnorm(n = 1e3, mu = matrix(1:6, nrow = 2, ncol = 3))
 
 matrix_draws[1, , ]
-#>          [,1]     [,2]     [,3]
-#> [1,] 1.570040 3.090437 3.874926
-#> [2,] 1.756116 3.012314 8.072248
+#>       [,1] [,2] [,3]
+#> [1,] 1.341 2.04 3.83
+#> [2,] 0.461 5.49 5.95
 ```
 
 Below is a simulation from a tensor variate normal of size
-$3 \times 4 \times 2$.
+$3 \times 4 \times
+2$.
 
 ``` r
 tensor_draws <- rtnorm(n = 1e3, mu = array(1:24, dim = c(3, 4, 2)))
@@ -109,20 +110,22 @@ tensor_draws <- rtnorm(n = 1e3, mu = array(1:24, dim = c(3, 4, 2)))
 tensor_draws[1, , , ]
 #> , , 1
 #> 
-#>          [,1]     [,2]     [,3]     [,4]
-#> [1,] 1.636847 2.465285 6.012573 11.69208
-#> [2,] 2.363168 4.173519 6.918542 10.61718
-#> [3,] 1.577817 5.582948 8.627753 12.00810
+#>       [,1] [,2] [,3] [,4]
+#> [1,] 0.979 2.94 6.89 10.1
+#> [2,] 1.409 4.73 7.59 10.2
+#> [3,] 3.130 5.57 9.95 12.0
 #> 
 #> , , 2
 #> 
-#>          [,1]     [,2]     [,3]     [,4]
-#> [1,] 12.89172 15.57493 18.23921 22.75339
-#> [2,] 14.21812 18.37944 21.06593 22.43802
-#> [3,] 14.67745 18.00590 20.13545 23.00758
+#>      [,1] [,2] [,3] [,4]
+#> [1,] 15.0 14.4 16.9 23.8
+#> [2,] 13.2 19.1 21.2 24.5
+#> [3,] 12.9 18.3 21.9 25.4
 ```
 
-# MLE Estimation for the Tensor Variate Normal
+# Estimation
+
+## MLE estimation for the tensor variate normal
 
 The package supports MLE estimation for the mean and list of covariance
 matrices.
@@ -131,80 +134,213 @@ matrices.
 univar_est <- mle_est(univar_draws)
 
 univar_est$mu
-#> [1] -1.984154
+#> [1] -2.12
+
 univar_est$sigmas
 #> [[1]]
-#>          [,1]
-#> [1,] 4.029426
+#>      [,1]
+#> [1,] 3.96
 ```
 
 Here, the mean matrix from the draws above has values 1 through 6. The
-covariance matrices are the identity matrices.
+covariance matrices are the identity matrices by default.
 
 ``` r
 matrix_est <- mle_est(matrix_draws)
-#> Iteration 1: max relative change = 1.228e-02
-#> Iteration 2: max relative change = 9.964e-05
-#> Iteration 3: max relative change = 4.082e-08
+#> Iteration 1: max relative change = 6.667e-01
+#> Iteration 2: max relative change = 3.827e-04
+#> Iteration 3: max relative change = 5.834e-07
 #> Converged at iteration 3
 
 matrix_est$mu
-#>           [,1]     [,2]     [,3]
-#> [1,] 0.9889842 2.993124 5.002082
-#> [2,] 1.9874096 3.980462 5.988387
-matrix_est$sigmas
+#>      [,1] [,2] [,3]
+#> [1,] 1.05 3.01 4.99
+#> [2,] 1.98 4.05 6.01
+
+matrix_est$sigmas |> lapply(round, 3)
 #> [[1]]
-#>             [,1]        [,2]
-#> [1,]  1.00308406 -0.01180287
-#> [2,] -0.01180287  0.99706430
+#>       [,1]   [,2]
+#> [1,]  1.02 -0.010
+#> [2,] -0.01  0.984
 #> 
 #> [[2]]
-#>             [,1]         [,2]         [,3]
-#> [1,] 1.007360206  0.007453601  0.008568699
-#> [2,] 0.007453601  0.990977958 -0.002157685
-#> [3,] 0.008568699 -0.002157685  1.001864848
+#>        [,1]   [,2]   [,3]
+#> [1,]  1.010  0.022 -0.010
+#> [2,]  0.022  0.970 -0.009
+#> [3,] -0.010 -0.009  1.020
 ```
 
 This function works on array objects of dimensions above 2.
 
 ``` r
 tensor_est <- mle_est(tensor_draws)
-#> Iteration 1: max relative change = 3.653e-02
-#> Iteration 2: max relative change = 7.607e-04
-#> Iteration 3: max relative change = 2.000e-06
-#> Iteration 4: max relative change = 9.828e-09
+#> Iteration 1: max relative change = 7.500e-01
+#> Iteration 2: max relative change = 9.260e-04
+#> Iteration 3: max relative change = 1.010e-05
+#> Iteration 4: max relative change = 3.509e-08
 #> Converged at iteration 4
 
 tensor_est$mu
 #> , , 1
 #> 
-#>           [,1]     [,2]     [,3]      [,4]
-#> [1,] 0.9950717 4.004322 6.964651  9.999908
-#> [2,] 1.9865597 4.991675 7.935281 10.977554
-#> [3,] 3.0780787 6.037056 9.005040 12.045153
+#>       [,1] [,2] [,3] [,4]
+#> [1,] 0.978 3.98 6.99   10
+#> [2,] 2.060 5.04 8.01   11
+#> [3,] 2.962 6.03 9.04   12
 #> 
 #> , , 2
 #> 
-#>          [,1]     [,2]     [,3]     [,4]
-#> [1,] 12.98197 16.04675 19.03532 22.01601
-#> [2,] 13.99639 16.97609 19.99714 23.00097
-#> [3,] 14.99562 18.02654 20.98631 24.05676
-tensor_est$sigmas
+#>      [,1] [,2] [,3] [,4]
+#> [1,] 12.9 16.0   19 21.9
+#> [2,] 14.0 17.0   20 23.0
+#> [3,] 15.0 18.1   21 24.0
+
+tensor_est$sigmas |> lapply(round, 3)
 #> [[1]]
-#>              [,1]        [,2]        [,3]
-#> [1,]  0.970626129 0.006219061 -0.01527996
-#> [2,]  0.006219061 1.015100266  0.02040243
-#> [3,] -0.015279957 0.020402435  1.01563140
+#>       [,1]  [,2]  [,3]
+#> [1,] 1.005 0.000 0.018
+#> [2,] 0.000 0.991 0.007
+#> [3,] 0.018 0.007 1.004
 #> 
 #> [[2]]
-#>              [,1]       [,2]         [,3]         [,4]
-#> [1,]  1.037256620 0.01974396 -0.005775892  0.002004411
-#> [2,]  0.019743960 0.97462151  0.019342349  0.015668624
-#> [3,] -0.005775892 0.01934235  1.002474777 -0.023123390
-#> [4,]  0.002004411 0.01566862 -0.023123390  0.988340795
+#>        [,1]   [,2]   [,3]   [,4]
+#> [1,]  1.039  0.007  0.007 -0.004
+#> [2,]  0.007  0.989 -0.020  0.004
+#> [3,]  0.007 -0.020  0.970  0.004
+#> [4,] -0.004  0.004  0.004  1.003
 #> 
 #> [[3]]
-#>              [,1]         [,2]
-#> [1,]  1.000059904 -0.006475362
-#> [2,] -0.006475362  0.999982027
+#>        [,1]   [,2]
+#> [1,]  1.006 -0.009
+#> [2,] -0.009  0.994
+```
+
+# Other models
+
+## Tensor variate generalized hyperbolic
+
+``` r
+genhyper_draws <- rtgenhyper(n = 1e3)
+```
+
+## Tensor variate variance gamma
+
+``` r
+vargamma_draws <- rtvargamma(n = 1e3)
+```
+
+## Tensor variate normal inverse Gaussian
+
+``` r
+invgauss_draws <- rtinvgauss(n = 1e3)
+```
+
+## Assessing tensor variate normality
+
+We can use the Mahalanobis distance to assess the tensor variate
+normality. If we compute the distances and plot them, they should be
+distributed as approximately $\chi_{p}$ where $p$ is the product of all
+the modes of the draws.
+
+``` r
+centered_draws <- matrix_draws - array(rep(matrix_est$mu, each = n), dim = dim(matrix_draws))
+
+inv_sqrt <- function(Sigma, tol = 1e-8) {
+  # syemmetric matrix
+  Sigma <- (Sigma + t(Sigma)) / 2
+  
+  eig <- eigen(Sigma, symmetric = TRUE)
+  vals <- pmax(eig$values, tol)  # positive eigenvalues
+  V <- eig$vectors
+  
+  Winv <- V %*% diag(1 / sqrt(vals), nrow = length(vals)) %*% t(V)
+  Winv
+}
+
+D2 <- rep(0, n)
+#> Error: cannot coerce type 'closure' to vector of type 'double'
+
+inv_sigmas <- vector("list", length(matrix_est$sigmas))
+
+for(k in 1:2) {
+  inv_sigmas[[k]] <- inv_sqrt(matrix_est$sigmas[[k]])
+}
+
+for(i in 1:n) {
+  Y <- centered_draws[i, , ]
+  for(k in 1:2) {
+    Y <- n_prod(Y, matrix_est$sigmas[[k]], k)
+  }
+  D2[i] <- sum(c(Y)^2)
+}
+#> Error in 1:n: NA/NaN argument
+
+sigma2_hat <- mean(D2) / p
+#> Error: object 'D2' not found
+D2_std <- D2 / sigma2_hat    
+#> Error: object 'D2' not found
+
+ggplot() +
+  geom_histogram(aes(x = D2_std, y = after_stat(density)), color = "black", fill = "pink", bins = 30) + 
+  geom_function(fun = dchisq, args = list(df = prod(dim(matrix_est$mu))), color = "red") +
+  theme_minimal() + 
+  labs(x = "Mahalanobis distance", y = "Density")
+#> Error in `geom_histogram()`:
+#> ! Problem while computing aesthetics.
+#> ℹ Error occurred in the 1st layer.
+#> Caused by error:
+#> ! object 'D2_std' not found
+```
+
+## Tensor Variate Skewed T
+
+``` r
+tibble(skew25 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 0.25, nu = 6)),
+       skew50 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 0.5, nu = 6)),
+       skew0 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 0, nu = 6)),
+       skew1 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 1, nu = 6)),
+       skew5 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 5, nu = 6)),
+       skew10 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 10, nu = 6))) |>
+  pivot_longer(cols = everything()) |>
+  ggplot() +
+  geom_histogram(aes(x = value), color = "black", fill = "pink") +
+  facet_wrap(~name, nrow = 2, scales = "free_x") +
+  theme_minimal()
+#> `stat_bin()` using `bins = 30`. Pick better value `binwidth`.
+```
+
+<img src="man/figures/README-unnamed-chunk-16-1.png" width="100%" />
+
+``` r
+
+tibble(nu05 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 1, nu = 0.5)),
+       nu1 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 1, nu = 1)),
+       nu5 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 1, nu = 5)),
+       nu10 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 1, nu = 10)),
+       nu20 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 1, nu = 20)),
+       nu50 = c(rtskewt(n = 1e3, mu = matrix(0, nrow = 2, ncol = 3), skew = 1, nu = 50))) |>
+  pivot_longer(cols = everything()) |>
+  ggplot() +
+  geom_histogram(aes(x = value), color = "black", fill = "pink") +
+  facet_wrap(~name, nrow = 2, scales = "free_x") +
+  theme_minimal()
+#> `stat_bin()` using `bins = 30`. Pick better value `binwidth`.
+```
+
+<img src="man/figures/README-unnamed-chunk-16-2.png" width="100%" />
+
+## Installation
+
+You can install the development version of tensormodels from
+[GitHub](https://github.com/) with:
+
+``` r
+if (!requireNamespace("remotes")) install.packages("remotes")
+remotes::install_github("ankan12/tensormodels")
+```
+
+Reset digits
+
+``` r
+options(digits = old_digits)
 ```
